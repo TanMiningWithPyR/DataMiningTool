f2b2e048be7ab50eae9a0afe5f30feb5851b5c46
Affluence Tan <fuyou_tan@sina.cn>
2018-07-25 11:04:08 +0800

    Multi Language
    English and Chinese and Query Design

--- R Script/Plot.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Program Files\Microsoft\R Open\R-3.4.0\bin\x64
-Rscript.exe "C:\Users\tanalan\Documents\Establish Table\R Script\Plot.r" "C:\Users\tanalan\Documents\Establish Table\Parameter\tblGlobalVar.xlsx"
-pause
