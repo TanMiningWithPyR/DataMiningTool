a653e40050bfe4a79050eae4771935377076a5b7
Affluence Tan <fuyou_tan@sina.cn>
2019-05-09 19:25:49 +0800

    web client start
    add mod webService

--- Web Server/hello.py
+++ Web Server/hello.py
@@ -1,9 +1,27 @@
 from flask import Flask
+from io import BytesIO, StringIO
+
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/data/<tablename>')
+def get_csv(tablename):
+    df_pd = pd.DataFrame({'aaa': [1,2,3],'aab':[4,5,6]})
+    
+    # Create buffer
+    csv_buffer = StringIO()
+    
+    # Write dataframe to buffer
+    df_pd.to_csv(csv_buffer, sep=";", index=False)
+    
+    return_string = csv_buffer.getvalue()
+    
+    return return_string
 
 if __name__ == '__main__':
     app.run()
